6513d6339ca65eaa6d4d3556945f7aecbf69a6b0
Eric Johnson <johnson.n.eric@gmail.com>
2020-04-10 03:18:34 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,6 +16,10 @@ def loop_message_n_times(message,n)
   end
 end
 
-def output_array
-  
+def output_array(array)
+  counter = 0
+  while counter < array.length do
+    puts array[counter]
+    counter += 1
+  end
 end
